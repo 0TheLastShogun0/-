cf9c901cbf714ae1e94956b3e74346af15944e56
0TheLastShogun0 <kostanovoselskij@gmail.com>
2025-02-18 10:52:05 +0300

    Add files via upload

---
+++ 4 семестр/java01.js
@@ -0,0 +1,12 @@
+'Привет, Java!'
+var myThing;
+myThing=5;
+myThing="эта строка";
+var numberNine=9;
+var stringNine="9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет, ";
+var myName = "TheLastShogun";
+greeting + myName;
+myName.length;
